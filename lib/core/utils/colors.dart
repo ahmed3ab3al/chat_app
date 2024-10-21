086e123fa1f364969239f3a4334ed5baa32c763a
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-10-21 16:47:59 +0300

    create app infrastructure

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorManager {
+  static const Color blackColor = Color(0xff000000);
+  static const Color blueColor0E4CA1 = Color(0xff0E4CA1);
+  static const Color greyColor757474 = Color(0xff757474);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color greyColorD9D9D9 = Color(0xffD9D9D9);
+  static const Color greyColorEEEEEE = Color(0xffEEEEEE);
+  static const Color redColorDC2222 = Color(0xffDC2222);
+}
